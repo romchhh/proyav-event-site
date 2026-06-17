c0383f625fee2c091a05713e9728d57998bea78d
nowayrm <your.email@example.com>
2026-06-17 11:55:53 +0300

    some updates

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
     serverComponentsExternalPackages: ['better-sqlite3'],
